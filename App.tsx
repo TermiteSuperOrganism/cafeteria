a7c301312d29f3f22c84619d97fb8d6c78dba074
TermiteSuperOrganism <tomasgalileu@gmail.com>
2026-09-08 16:55:14 -0300

    atualiza

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
@@ -22,13 +22,20 @@ export default function App() {
           <Text style={styles.bodyTitle}>Bom dia!</Text>
           <Text style={styles.bodySubtitle}>Que tal um café hoje?</Text>
         </View>
+
+        <View style={styles.featured}>
+          <Image source={require('./assets/coffee.jpg')} style={styles.image}></Image>
+          <Text>Cappucino Especial</Text>
+          <Text>Cremoso e delicioso</Text>
+          <Text>R$ 12,90</Text>
+        </View>
       </View>
       {/*Body*/}
     </ScrollView>
   );
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f9f9f9'
@@ -42,41 +49,55 @@ const styles=StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center'
   },
-  headerTitle:{
-    fontSize:22,
-    fontWeight:'800',
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: '800',
     color: "#2f2d2c"
   },
-  headerSubtitle:{
-    fontSize:14,
-    color:"#9b9b9b",
-    marginTop:4
+  headerSubtitle: {
+    fontSize: 14,
+    color: "#9b9b9b",
+    marginTop: 4
   },
-  avatarPlaceholder:{
-    width:44,
+  avatarPlaceholder: {
+    width: 44,
     height: 44,
-    borderRadius:22,
-    backgroundColor:"#f0f0f0",
-    justifyContent:"center",
-    alignItems:"center"
+    borderRadius: 22,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  body:{
-    width: '100%',
+  body: {
     paddingHorizontal: 24,
     paddingBottom: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+
   },
-  bodyTitle:{
-    fontSize:32,
-    fontWeight:"800",
+  bodyTitle: {
+    fontSize: 32,
+    fontWeight: "800",
     color: "#2f2d2c"
   },
-  bodySubtitle:{
-    fontSize:16,
-    color:"#9b9b9b",
-    marginTop:8
+  bodySubtitle: {
+    fontSize: 16,
+    color: "#9b9b9b",
+    marginTop: 8
+  },
+  featured: {
+    backgroundColor: "#ffffff",
+    borderRadius: 16,
+    padding: 16,
+    marginBottom: 32,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.05,
+    elevation: 4,
+    marginTop:20
+  },
+  image: {
+    width: "100%",
+    height: 188,
+    borderRadius: 16,
+    marginBottom: 16
   }
 })
 
